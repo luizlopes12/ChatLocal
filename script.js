014c15d6372955398cc2a903f068c683cb654e8c
Luiz Lopes <luizlopes12@outlook.com>
2021-06-22 17:05:31 -0300

    sla

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function adicionar(){
-    var text = document.getElementById('add').value;    
+    var text = document.getElementById('add').value;
+    if(text.length > 0){   
     var lista = document.getElementById('itens').innerHTML;
     lista += "<li>"+text+"</li>";
     document.getElementById('itens').innerHTML = lista;
+    }
 }
 function limpar(){
     window.location.href = window.location.href;
